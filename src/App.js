0053b458f032ae6a3cfdb72b9d5ea3b623c309b2
Grazziano Borges Fagundes <grazzianofagundes@gmail.com>
2019-08-23 21:25:11 -0300

    Instalando o react-router-dom e ajustando o App.js

--- src/App.js
+++ src/App.js
@@ -3,14 +3,17 @@ import Home from './Home'
 import FormCadastro from './FormCadastro'
 import Listagem from './Listagem';
 import Resumo from './Resumo';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 function App() {
   return (
     <div>
-      <Home />
-      <FormCadastro />
-      <Listagem />
-      <Resumo />
+      <Router>
+        <Route path="/" exact component={Home} />
+        <Route path="/cadastro/" component={FormCadastro} />
+        <Route path="/listagem/" component={Listagem} />
+        <Route path="/resumo/" component={Resumo} />
+      </Router>
     </div>
   );
 }
