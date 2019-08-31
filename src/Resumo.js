be56be7726d762c86b69af091d2e2974acdc671e
Grazziano Borges Fagundes <grazzianofagundes@gmail.com>
2019-08-30 21:11:48 -0300

    Resumo: total de filmes

--- src/Resumo.js
+++ src/Resumo.js
@@ -1,13 +1,30 @@
 import React from 'react'
 
 class Resumo extends React.Component {
+
+    state = {
+        total: 0,
+        media: 0,
+        generosNum: []
+    }
+
+    componentDidMount() {
+        this.loadFilmes()
+    }
+
+    loadFilmes = async () => {
+        const filmes = await JSON.parse(localStorage.getItem('filmes'))
+
+        this.setState({ total: filmes.length })
+    }
+
     render() {
         return (
             <div className="media border p-3 mx-2">
                 <img src="resumo.jpg" alt="Resumo" className="mr-3 mt-3 rounded-circle" style={{ width: 80 }} />
                 <div className="media-body">
                     <h4 className="text-danger">Resumo: Filmoteca</h4>
-                    <h5><i>Total de Filmes: </i></h5>
+                    <h5><i>Total de Filmes: {this.state.total}</i></h5>
                     <h5><i>Média de Notas: </i></h5>
                     <h5 className="text-primary">Filmes por Gênero: </h5>
                 </div>
